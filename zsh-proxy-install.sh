be3ef2d2a74a0c3a548270032afdde5b8f030cfb
niexiaorong <fukua365@gmail.com>
2025-06-27 11:23:25 +0800

    fix(插件启用): 增加zsh-motd

--- zsh-proxy-install.sh
+++ zsh-proxy-install.sh
@@ -139,7 +139,7 @@ else
 # ===== ZSH CONFIG BLOCK =====
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="random"
-plugins=(git z docker docker-compose zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(git z docker docker-compose zsh-autosuggestions zsh-syntax-highlighting zsh-motd)
 source $ZSH/oh-my-zsh.sh
 # ===== END OF CONFIG BLOCK =====
 EOL
